941cd064df0edbc62d9a389c28815f173c6e2b69
clod44 <Rt_Sayochi@hotmail.com>
2024-12-28 01:43:49 +0300

    virtual list

--- src/components/NotesList/NoteCard.jsx
+++ src/components/NotesList/NoteCard.jsx
@@ -1,18 +1,25 @@
 import { Card, Text, Flex, Box } from "@radix-ui/themes";
 import NoteCardDropdown from "./NoteCardDropdown";
 
-const NoteCard = () => {
+
+
+const NoteCard = ({
+    note,
+    index,
+    className,
+    ...props
+}) => {
     return (
-        <Card variant="classic" className="shadow-lg">
+        <Card variant="classic" className={"shadow-lg " + className} {...props}>
             <Flex gap="3" align={"center"} justify={"between"}>
                 <Box>
-                    <Text as="div" size="3" weight="bold">Quick start</Text>
+                    <Text as="div" size="3" weight="bold">Quick start {index + 1}</Text>
                     <Text as="div" size="1" weight={"light"} className="text-neutral-500">{new Date().toDateString()}</Text>
                 </Box>
                 <NoteCardDropdown />
             </Flex>
             <Text as="div" color="gray" size="2">
-                Start building your next project in minutes
+                Start building your next project in minutes ...
             </Text>
         </Card>
     )
--- src/components/NotesList/NotesList.jsx
+++ src/components/NotesList/NotesList.jsx
@@ -1,8 +1,42 @@
-import { useState, useEffect, useTransition } from "react";
-import { Grid, ScrollArea, Spinner, Flex, Text } from "@radix-ui/themes";
+import { useState, useEffect, useTransition, forwardRef } from "react";
+import { ScrollArea, Spinner, Flex, Text } from "@radix-ui/themes";
 import NoteCard from "./NoteCard.jsx";
 import { useSettings } from "../../context/SettingsContext.jsx";
+import { FixedSizeList as List } from 'react-window';
+import AutoSizer from "react-virtualized-auto-sizer";
 
+
+const PADDING_SIZE = 60; //this gets applied both top and bottom
+const NoteCardWrapper = ({
+    note,
+    index,
+    style,
+    className = "p-3 py-1",
+    ...props
+}) => {
+    return (
+        <div
+            className={className}
+            style={{
+                ...style,
+                top: `${parseFloat(style.top) + PADDING_SIZE}px`
+            }}
+        >
+            <NoteCard index={index} note={note} className="w-full h-full" />
+        </div>
+    )
+}
+
+const innerElementType = forwardRef(({ style, ...rest }, ref) => (
+    <div
+        ref={ref}
+        style={{
+            ...style,
+            height: `${parseFloat(style.height) + PADDING_SIZE * 2}px`
+        }}
+        {...rest}
+    />
+));
 
 const NotesList = () => {
     const { listViewType } = useSettings();
@@ -14,7 +48,7 @@ const NotesList = () => {
         setLoading(true);
         setTimeout(() => {
             startTransition(() => {
-                setNotes(Array.from({ length: 20 }, (_, index) => index));
+                setNotes(Array.from({ length: 200 }, (_, index) => index));
             });
             setLoading(false);
         }, 200);
@@ -29,18 +63,20 @@ const NotesList = () => {
                         <Text size={"1"} className="text-neutral-500">Artificial Delay...</Text>
                     </Flex>
                 ) : (
-                    <Grid
-                        columns={listViewType === "list" ? "1" : "2"}
-                        gap="3"
-                        rows="auto"
-                        width="100%"
-                        p={"3"}
-                        className="animate-in fade-in pt-16 pb-64"
-                    >
-                        {notes.map((note, index) => (
-                            <NoteCard key={index} />
-                        ))}
-                    </Grid>
+                    <AutoSizer>
+                        {({ height, width }) => (
+                            <List
+                                height={height}
+                                itemCount={notes.length}
+                                innerElementType={innerElementType}
+                                itemSize={100}
+                                width={width}
+                                className="pb-52" //here only pb-* works properly for some reason
+                            >
+                                {NoteCardWrapper}
+                            </List>
+                        )}
+                    </AutoSizer>
                 )}
             </ScrollArea>
         </>
