c2e36b1bbe504768cc28cb1ce140c2da2ca68a5f
clod44 <Rt_Sayochi@hotmail.com>
2024-12-27 17:11:13 +0300

    hold notes in state

--- src/components/NotesList/NotesList.jsx
+++ src/components/NotesList/NotesList.jsx
@@ -1,25 +1,51 @@
-import { Grid, ScrollArea } from "@radix-ui/themes";
+import { useState, useEffect, useTransition, useRef } from "react";
+import { Grid, ScrollArea, Spinner, Flex, Text } from "@radix-ui/themes";
 import NoteCard from "./NoteCard.jsx";
 import { useSettings } from "../../context/SettingsContext.jsx";
 
+
 const NotesList = () => {
     const { listViewType } = useSettings();
+    const [notes, setNotes] = useState([]);
+    const [isPending, startTransition] = useTransition();
+    const [loading, setLoading] = useState(true);
+
+
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            startTransition(() => {
+                setNotes(Array.from({ length: 20 }, (_, index) => index));
+            });
+            setLoading(false);
+        }, 200);
+    }, []);
+
     return (
-        <ScrollArea>
-            <Grid
-                columns={listViewType === "list" ? "1" : "2"}
-                gap="3"
-                rows="auto"
-                width="auto"
-                p={"4"}
-                className="pb-52 pt-16"
-            >
-                {Array.from({ length: 20 }, (_, index) => index).map((index) => (
-                    <NoteCard key={index} />
-                ))}
-            </Grid>
-        </ScrollArea>
+        <>
+            <ScrollArea>
+                {(isPending || loading) ? (
+                    <Flex justify={"center"} align={"center"} height={"100%"} gap="3" direction={"column"}>
+                        <Spinner />
+                        <Text size={"1"} className="text-neutral-500">Artificial Delay...</Text>
+                    </Flex>
+                ) : (
+                    <Grid
+                        columns={listViewType === "list" ? "1" : "2"}
+                        gap="3"
+                        rows="auto"
+                        width="100%"
+                        p={"3"}
+                        className="animate-in fade-in pt-16 pb-64"
+                    >
+                        {notes.map((note, index) => (
+                            <NoteCard key={index} />
+                        ))}
+                    </Grid>
+                )}
+            </ScrollArea>
+        </>
     );
 };
 
-export default NotesList
+export default NotesList;
