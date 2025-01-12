5c0b8af7fa5fe0ec6a7ee7a102a765c94c0f668e
clod44 <Rt_Sayochi@hotmail.com>
2025-01-12 22:15:17 +0300

    fix topbar choosing

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -4,18 +4,11 @@ import { HomeBar, GenericBar, NoteBar } from "./Bars";
 import { IoFolderOpen } from "react-icons/io5";
 import { FaTasks } from "react-icons/fa";
 
+
 const TopBar = () => {
     const { pathname } = useLocation();
-    /*
-        Order of these bars is important!
-        it should be from most complicated to least
-        eg if you put "/" at the top, it will always choose it first because "/something" starts with "/"
-        
-        1- "/something/specific"
-        2- "/something"
-        3- "/another"
-        4- "/"
-    */
+
+    //list the paths from most specified to least
     const barConfig = [
         { path: "/folders", component: <GenericBar title="Folders" icon={<IoFolderOpen className="size-6 text-gray-500" />} /> },
         { path: "/todo", component: <GenericBar title="To Do" icon={<FaTasks className="size-6 text-gray-500" />} /> },
@@ -23,10 +16,11 @@ const TopBar = () => {
         { path: "/", component: <HomeBar /> },
     ];
 
-    const bar = barConfig.find(({ path }) => pathname.startsWith(path));
+    const bar = barConfig.find(({ path }) =>
+        pathname === path || pathname.startsWith(`${path}/`)
+    );
 
-    return bar ? bar.component : <GenericBar />;
+    return bar ? bar.component : <GenericBar title="Not Found" />;
 };
-
 
 export default TopBar;
